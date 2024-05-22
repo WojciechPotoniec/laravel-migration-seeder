b86f76cb1a694b73b339a3a0122437a5113e1dc0
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-22 19:04:10 +0200

    created function index

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::all();
+        // dd($trains);
+        $trains = Train::where('orario_di_partenza', '=', '2024-05-22 10:51:16')->get();
+        return view('home', compact('trains'));
+    }
 }
