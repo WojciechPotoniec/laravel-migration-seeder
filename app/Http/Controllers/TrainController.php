6cb4516d1a78cf13bc4dca2130507fa9fe01b6a1
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-23 14:18:41 +0200

    update query string

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -10,7 +10,7 @@ class TrainController extends Controller
     public function index(){
         $trains = Train::all();
         // dd($trains);
-        $trains = Train::where('orario_di_partenza', '=', '2024-05-22 10:51:16')->get();
+        $trains = Train::where('orario_di_partenza', '>=', now())->get();
         return view('home', compact('trains'));
     }
 }
